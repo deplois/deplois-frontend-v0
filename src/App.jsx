a40af1f8eb5b1c0811fe954e1166036d3f797dfc
dodo07070707 <aa01056328583@gmail.com>
2025-12-02 17:57:34 +0900

    1202 final commit
    Co-authored-by: Hunji <devhooni@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import FooterDesktop from "./widgets/desktop/footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  /* 모바일 */
+  const getScreenSize = () => {
+    const width = window.innerWidth;
+    if (width <= 480) return "mobile";
+    return "desktop";
+  };
+
+  const [screenSize, setScreenSize] = useState(getScreenSize());
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(getScreenSize());
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="app-container">
+            {screenSize === "mobile" ? <FooterDesktop /> : <FooterDesktop />}
+          </div>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
